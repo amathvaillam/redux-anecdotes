fa5e86773a45940ac8c2b9c0fcca8c83dab10da0
amath vaillam <amathv01@gmail.com>
2020-10-07 11:43:52 +0000

    6.9 Better anecdotes, step7

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import store from './redux-store'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+
+
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={ store }>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById( 'root' )
 )
