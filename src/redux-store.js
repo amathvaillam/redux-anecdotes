eb7a38269b6968d5d74171c1e148699444f23969
amath vaillam <amathv01@gmail.com>
2020-10-14 11:31:53 +0000

    6.15 Anecdotes and the backend, step3

--- src/redux-store.js
+++ src/redux-store.js
@@ -1,13 +1,16 @@
-import { createStore,combineReducers } from 'redux'
+import { createStore,combineReducers,applyMiddleware } from 'redux'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 const store = createStore( combineReducers( {
     anecdotes: anecdoteReducer,
     notification: notificationReducer,
     filter: filterReducer
-} ),composeWithDevTools() )
+} ),composeWithDevTools(
+    applyMiddleware( thunk )
+) )
 
 export default store;
